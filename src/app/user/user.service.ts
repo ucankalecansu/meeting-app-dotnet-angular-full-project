eb4bcb636a73207805142916e4947e5aba973b13
ucankalecansu <ucankalecansu@gmail.com>
2025-08-23 15:34:16 +0300

    user service yazildi

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+    private apiUrl = 'http://localhost:4000/api/users';
+
+    constructor(private http: HttpClient, private auth: AuthService) {}
+
+    private getAuthHeaders() {
+        const token = this.auth.getToken();
+        return {
+          headers: new HttpHeaders({
+            Authorization: `Bearer ${token}`
+          })
+        };
+      }
+
+    getUsers():Observable<any[]> {
+        return this.http.get<any[]>(this.apiUrl,this.getAuthHeaders());
+    }
+}
